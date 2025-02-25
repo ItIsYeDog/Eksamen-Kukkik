8add91c161668144087dab3093a27e12b1780851
itisyedogdev <itisyedogdev@gmail.com>
2025-02-25 13:52:56 +0100

    fixing some bugs

--- middleware/notFoundHandler.js
+++
@@ -1,8 +0,0 @@
-const notFoundHandler = (req, res) => {
-    res.status(404).render('error', {
-        statusCode: 404,
-        error: 'Siden ble ikke funnet'
-    });
-};
-
-module.exports = notFoundHandler;
--- routers/reinsdyrRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isAuth } = require('../middleware/authMiddleware');
-const reinsdyrController = require('../controllers/reinsdyrController');
-
-router.get('/register', isAuth, reinsdyrController.getRegisterPage);
-router.post('/register', isAuth, reinsdyrController.registerReinsdyr);
-
-module.exports = router;
